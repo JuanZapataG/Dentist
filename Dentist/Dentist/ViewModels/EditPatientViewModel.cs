d42730b80ad96205539b82be0ad186859dab319c
JuanZapataG <31329096+JuanZapataG@users.noreply.github.com>
2018-09-24 18:41:55 -0500

    sexto
    listo el cargue del delete

--- Dentist/Dentist/ViewModels/EditPatientViewModel.cs
+++ Dentist/Dentist/ViewModels/EditPatientViewModel.cs
@@ -12,6 +12,52 @@
 
     public class EditPatientViewModel : BaseViewModel
     {
+    
+
+        #region Attributes
+        private Patient patient;
+        private ApiService apiService;
+        private bool isRunning;
+        private bool isEnabled;
+        private ImageSource imageSource;
+        private MediaFile file;
+
+        #endregion
+
+        #region Properties
+        public Patient Patient
+        {
+            get { return this.patient; }
+            set { this.SetValue(ref this.patient, value); }
+        }
+        public bool IsEnabled
+        {
+            get { return this.isEnabled; }
+            set { this.SetValue(ref this.isEnabled, value); }
+        }
+        public bool IsRunning
+        {
+            get { return this.isRunning; }
+            set { this.SetValue(ref this.isRunning, value); }
+        }
+        public ImageSource ImageSource
+        {
+            get { return this.imageSource; }
+            set { this.SetValue(ref this.imageSource, value); }
+        }
+        #endregion
+
+        #region Constructors
+        public EditPatientViewModel(Patient Patient)
+        {
+            this.patient = Patient;
+            this.ImageSource = Patient.ImageFullPath;
+            this.apiService = new ApiService();
+            this.IsEnabled = true;
+            
+        }
+        #endregion
+
         #region Methods
         private async void Save()
         {
@@ -79,7 +125,7 @@
             var url = Application.Current.Resources["UrlAPI"].ToString();
             var prefix = Application.Current.Resources["UrlPrefix"].ToString();
             var patientsController = Application.Current.Resources["UrlPatientsController"].ToString();
-            var response = await this.apiService.Put(url, prefix, patientsController, patient,patient.PatientId);
+            var response = await this.apiService.Put(url, prefix, patientsController, patient, patient.PatientId);
             if (!response.IsSuccess)
             {
                 this.IsRunning = false;
@@ -150,50 +196,6 @@
             await Application.Current.MainPage.Navigation.PopToRootAsync();
 
 
-        }
-        #endregion
-
-        #region Attributes
-        private Patient patient;
-        private ApiService apiService;
-        private bool isRunning;
-        private bool isEnabled;
-        private ImageSource imageSource;
-        private MediaFile file;
-
-        #endregion
-
-        #region Properties
-        public Patient Patient
-        {
-            get { return this.Patient; }
-            set { this.SetValue(ref this.patient, value); }
-        }
-        public bool IsEnabled
-        {
-            get { return this.isEnabled; }
-            set { this.SetValue(ref this.isEnabled, value); }
-        }
-        public bool IsRunning
-        {
-            get { return this.isRunning; }
-            set { this.SetValue(ref this.isRunning, value); }
-        }
-        public ImageSource ImageSource
-        {
-            get { return this.imageSource; }
-            set { this.SetValue(ref this.imageSource, value); }
-        }
-        #endregion
-
-        #region Constructors
-        public EditPatientViewModel(Patient patient)
-        {
-            this.patient = patient;
-            this.ImageSource = patient.ImageFullPath;
-            this.apiService = new ApiService();
-            this.IsEnabled = true;
-            
         }
         #endregion
 
